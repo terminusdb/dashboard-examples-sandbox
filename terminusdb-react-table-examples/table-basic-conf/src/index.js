674142e7927fdcf668f53def6ff468b1ae56f100
Francesca-Bit <francesca@terminusdb.com>
2023-05-04 12:55:10 +0100

    add terminusdb-react-table-examples

--- terminusdb-react-table-examples/table-basic-conf/src/index.js
+++ terminusdb-react-table-examples/table-basic-conf/src/index.js
@@ -1,7 +1,7 @@
 import { createRoot } from "react-dom/client";
 
 import App from "./App";
-
+import "./App.css"
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
