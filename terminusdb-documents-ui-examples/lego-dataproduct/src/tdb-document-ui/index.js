91cda70d9c4bf5b81eb9983f304c580db7a15bbf
Francesca-Bit <francesca@terminusdb.com>
2023-05-04 21:49:37 +0100

    add document-ui like app

---
+++ terminusdb-documents-ui-examples/lego-dataproduct/src/tdb-document-ui/index.js
@@ -0,0 +1,5 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/terminusdb__styles.css';
+
+export { FrameViewer } from './FrameViewer'
+export { DiffViewer } from './diffs/diffViewer'
