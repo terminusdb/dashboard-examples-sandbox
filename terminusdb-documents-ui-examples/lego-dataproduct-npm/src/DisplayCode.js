0310bc9a0a043884c2ac95d3fefc4e33aa166369
Francesca-Bit <francesca@terminusdb.com>
2023-05-09 10:12:42 +0100

    review

--- terminusdb-documents-ui-examples/lego-dataproduct-npm/src/DisplayCode.js
+++ terminusdb-documents-ui-examples/lego-dataproduct-npm/src/DisplayCode.js
@@ -1,9 +1,12 @@
 import React, {useState} from 'react'
-import SyntaxHighlighter from 'react-syntax-highlighter'
-import { vs2015 } from 'react-syntax-highlighter/dist/esm/styles/hljs'
+//import SyntaxHighlighter from 'react-syntax-highlighter'
+//import { vs2015 } from 'react-syntax-highlighter/dist/esm/styles/hljs'
 
 export const DisplayCode = ({ codeString }) => {
-	return <SyntaxHighlighter language="javascript" style={vs2015} wrapLines={true}>
-		{codeString}
-	</SyntaxHighlighter>
+	return <pre>{codeString}</pre>
 }
+
+/*
+return <SyntaxHighlighter language="javascript" style={vs2015} wrapLines={true}>
+		{codeString}
+	</SyntaxHighlighter>*/
